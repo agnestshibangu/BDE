35f5fced8f95b26abe5f26fd068bafb46adc258a
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2023-12-11 22:42:14 +0100

    last commit for today

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,7 +5,11 @@ import '../Style/section.scss';
 export default function Section() {
     return (
         <div className="main-section">
-            
+            <h2 className="section-title">Lorem ipsum</h2>
+            <p className="section-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum at varius vel pharetra vel turpis nunc eget lorem. Dignissim enim sit amet venenatis urna cursus. Urna et pharetra pharetra massa. Amet mattis vulputate enim nulla aliquet. Vivamus arcu felis bibendum ut tristique et. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Neque convallis a cras semper. Velit egestas dui id ornare.
+            Id consectetur purus ut faucibus pulvinar. Orci a scelerisque purus semper eget duis at. Adipiscing diam donec adipiscing tristique risus nec. Venenatis cras sed felis eget. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Nisl suscipit adipiscing bibendum est. Nibh tortor id aliquet lectus proin nibh. Duis at tellus at urna condimentum mattis pellentesque id. Nunc vel risus commodo viverra maecenas. Aliquet lectus proin nibh nisl condimentum id venenatis. Urna cursus eget nunc scelerisque viverra mauris in. Et ultrices neque ornare aenean euismod elementum. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue.
+            </p>
         </div>
     )
 }

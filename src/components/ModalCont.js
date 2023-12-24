058807d257adfb0236883d8bf7a1800a25fb48c4
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2023-12-24 01:48:19 +0100

    modal anim

--- src/components/ModalCont.js
+++ src/components/ModalCont.js
@@ -7,29 +7,19 @@ export default function ModalContent() {
           <div className='geometry-box'>
             <div className='vertical-line'/>
             <div className='round-half'/>
-            <div className='round'/>       
+            <div className='round'/>
           </div>
+      <div className="circle"></div>
+      <div className="circle2"></div>
+      <div className="circle3"></div> 
+      <div className="circle4"></div>
+      {/* <div className="circle5"></div>
+      <div className="circle6"></div>
+      <div className="circle7"></div>  */}
 
-          <div class="circle-container">
-            <div class="circle">1</div>
-            <div class="circle">2</div>
-            <div class="circle">3</div>
-            <div class="circle">4</div>
-            <div class="circle">5</div>
-            <div class="circle">6</div>
-            <div class="circle">7</div>            
-          </div>
 
-          <div class="circle-container serie2">
-            <div class="circle-serie2">1</div>
-            <div class="circle-serie2">2</div>
-            <div class="circle-serie2">3</div>
-            <div class="circle-serie2">4</div>
-            <div class="circle-serie2">5</div>
-            <div class="circle-serie2">6</div>
-            <div class="circle-serie2">7</div>            
-          </div>
-                  
+
+
     </div>
   )
 }

88819f6ec55b3f4dd5faf72f94a56546789a3ed4
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2024-01-04 12:15:00 +0100

    modal wip

--- src/components/ModalCont.js
+++ src/components/ModalCont.js
@@ -9,10 +9,21 @@ export default function ModalContent() {
             <div className='round-half'/>
             <div className='round'/>
           </div>
-      <div className="circle"></div>
-      <div className="circle2"></div>
-      <div className="circle3"></div> 
-      <div className="circle4"></div>
+      <div className='circles-box'>
+        <div className="circle"></div>
+        <div className="circle2 circle"></div>
+        <div className="circle3 circle"></div> 
+        <div className="circle4 circle"></div>
+        <div className="circle5 circle"></div>
+        <div className="circle6 circle"></div> 
+        <div className="circle7 circle"></div>
+        <div className="circle8 circle"></div>
+      </div>    
+
+      <div className='plane-div'>
+        
+      </div>
+      
       {/* <div className="circle5"></div>
       <div className="circle6"></div>
       <div className="circle7"></div>  */}
